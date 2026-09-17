c847dc9ce83287ba87150c1b0913d747dfde335a
EstherFaBer <estherfaundez@gmail.com>
2026-09-17 22:56:58 +0100

    setup

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,17 +1,1 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-
-
-const app = express()
-const PORT = process.env.PORT ?? 4000
-
-// Middleware
-
-app.use(express.json())
-
-
-exports default app
-
-
 
